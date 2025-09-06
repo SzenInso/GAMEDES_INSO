3b66f5d91024cae5c4f0d655e498fe302fec0d2f
Fhrenne Szen Inso <inso_f@f112.ub.lan>
2025-09-06 10:12:44 +0800

    added changes made at lab class

--- spacewar/run_baby.gd
+++ spacewar/run_baby.gd
@@ -1,17 +1,22 @@
-extends Node2D
+extends CharacterBody2D
 
 func _ready() -> void:
+	var rng := RandomNumberGenerator.new()
 	# Starting position
-	position = Vector2(850, 300)
-
+	#position = Vector2(850, 300)
+	var width = get_viewport().get_visible_rect().size[0]
+	var random_x = rng.randi_range(0, width)
+	var random_y = rng.randi_range(150, 50)
+	position = Vector2(random_x, random_y)
+	
 func _process(delta):
 	# Get screen size
-	var screen_size = get_viewport_rect().size
+	#var _screen_size = get_viewport_rect().size
 
 	# Apply movement first
 	position += Vector2(-.5, .75) * 60 * delta
 	rotation += (.5) * 1 * delta
 
-	# Then clamp the position to keep it within bounds
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+func _on_body_entered(body):
+	print ('body entered')
+	print(body)
